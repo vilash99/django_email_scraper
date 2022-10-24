ae35148fdca4379b469efbaf990212856e47be27
Vilashkumar Daate <vdaate@gmail.com>
2022-10-24 09:51:47 +0530

    Added URL check method
    Added is_valid_url function to check if the given URL is valid or not. If it is not valid then the INVALID URL will be returned.

--- scraper/views.py
+++ scraper/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.http import JsonResponse
 import re
 import requests
+from urllib.parse import urlparse
 
 def index(request):
     """
@@ -23,13 +24,25 @@ def extract_email_ajax(request):
                         (\.[a-zA-Z]{2,4}))''', re.VERBOSE)
 
         urls = request.GET.get('URLS')
-
-        page_data = requests.get(urls)
-        page_html = str(page_data.content)
         matches = ""
 
-        for groups in email_regex.findall(page_html):
-            if matches.find(groups[0]) == -1:
-                matches += groups[0] + ', '
+        if is_valid_url(urls) == True:
+            try:
+                page_data = requests.get(urls)
+                page_html = str(page_data.content)
+            except:
+                page_html = ""
 
+            for groups in email_regex.findall(page_html):
+                if matches.find(groups[0]) == -1:
+                    matches += groups[0] + ', '
+        else:
+            matches = "INVALID URL"
         return JsonResponse({'extracted_emails': matches, 'for_url': urls})
+
+def is_valid_url(url):
+    """
+    Checks whether 'url' is a valid URL.
+    """
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
